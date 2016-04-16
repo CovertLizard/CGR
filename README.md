f10a196bf8c0017a4085b505d43b7f4648313b7e
CovertLizard <CovertLizard@users.noreply.github.com>
2016-04-16 15:18:46 -0400

    Initial project setup

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Core Games Reboom</h1>
+<hr>
+<p>Core Games Reboom (or CGR) is a remake of the popular Hunger Games flavor "Core Games".</p>
+<h2 style="color:yellow; background: black;">!!CURRENTLY A WIP!!</h2>
+<hr>
---
+++ src/main/java/com/covertlizard/cgr/Core.java
@@ -0,0 +1,18 @@
+package com.covertlizard.cgr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/****************************************************
+ * Created: 4/16/2016 at 2:45 PM by CovertLizard
+ * FQN: com.covertlizard.cgr.Core
+ * Info: Core class for the CGR plugin
+ ****************************************************/
+@SuppressWarnings("all")
+public class Core extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        this.getLogger().info("Project template.");
+    }
+}
